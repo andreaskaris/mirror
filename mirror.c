fe38ec871a4d191632dc374fa703474e2e68c30b
kreeze <ak.karis@gmail.com>
2013-01-18 02:13:24 -0500

    Can now send messages from UDP client to UDP server

--- mirror.c
+++ mirror.c
@@ -5,6 +5,8 @@
 #include <sys/socket.h>
 #include <netinet/ip.h>
 #include <arpa/inet.h>
+#include <strings.h>
+#include <errno.h>
 
 #define H_TEXT "mirror [-t|-u] [-s|-c] dst-addr port\n" \
 "\n" \
@@ -13,12 +15,14 @@
 "-u UDP\n" \
 "-h This help text\n\n"
 
+#define BUF_LEN 1024
+
 void helptext();
 void readargs(int argc, char *argv[], char *proto, char *mode, unsigned int *address, unsigned short *port, char * default_port);
 void udp_server(const unsigned short port);
 void tcp_server(const unsigned short port);
-void udp_client(const unsigned int addr, const unsigned short port);
-void tcp_client(const unsigned int addr, const unsigned short port);
+void udp_client(const unsigned int addr, const unsigned short port, int fd);
+void tcp_client(const unsigned int addr, const unsigned short port, int fd);
 
 int main(int argc, char *argv[]) {
   char proto, mode;
@@ -36,9 +40,9 @@ int main(int argc, char *argv[]) {
     }
   } else {
     if(proto == 'u') {
-      udp_client(addr, port);
+      udp_client(addr, port, 0);
     } else {
-      tcp_client(addr, port);
+      tcp_client(addr, port, 0);
     }
   }
 
@@ -128,23 +132,44 @@ void helptext() {
 void udp_server(const unsigned short port) {
   printf("creating udp server on port %d\n", port);
 
-  int sock;
+  int sock, addrlen;
+  char buf[BUF_LEN];
   struct sockaddr_in server_addr;
+  struct sockaddr_in client_addr;
+  unsigned int client_addr_size;
+
   unsigned short nport = htons(port); //port in network byte order
-  printf("%x, %x\n",port, nport);
+  
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = nport;
   server_addr.sin_addr.s_addr = INADDR_ANY;
-  
+
   if( (sock = socket(AF_INET, SOCK_DGRAM, 0) ) == - 1) { //open a UDP socket
     perror("Creating socket");
+    exit(1);
   }
   if( bind(sock, (const struct sockaddr *) &server_addr, sizeof(server_addr)) == -1 ) {
     perror("Binding socket to name");
+    exit(1);
   }
-  
-  sleep(10);
 
+  for( ;; ) { //run until break
+    printf("in the loop\n");
+    bzero(buf, sizeof(buf));
+    bzero(&client_addr, sizeof(client_addr));
+    client_addr_size = sizeof(client_addr);
+    addrlen = recvfrom(sock, buf, sizeof(buf), 0, 
+	     (struct sockaddr *) &client_addr, &client_addr_size
+	     );
+    if( addrlen == -1 ) {
+      perror("receiving message");
+      exit(1);
+    }
+
+    printf("%s\n", buf);
+  }
+
+  close(sock);
 }
 
 /*
@@ -157,14 +182,52 @@ void tcp_server(const unsigned short port) {
 /*
  * Create a UDP client
  */
-void udp_client(const unsigned int addr, const unsigned short port) {
+void udp_client(const unsigned int addr, const unsigned short port, int fd) {
   printf("creating udp client, dst %d.%d.%d.%d and dport %d\n", addr >> 24 & 255, addr >> 16 & 255, addr >> 8 & 255, addr & 255, port);
+
+  int sock; 
+  //int addrlen;
+  char buf[BUF_LEN];
+  struct sockaddr_in server_addr;
+  //struct sockaddr_in client_addr;
+  //unsigned int client_addr_size;
+
+  unsigned int naddr = htonl(addr);
+  unsigned short nport = htons(port); //port in network byte order
+  
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = nport;
+  server_addr.sin_addr.s_addr = naddr;
+
+  if( (sock = socket(AF_INET, SOCK_DGRAM, 0) ) == - 1) { //open a UDP socket
+    perror("Creating socket");
+    exit(1);
+  }
+
+  for(;;) {
+    bzero(buf, sizeof(buf));
+    if( read(fd, buf, sizeof(buf)) == -1 ) {
+      printf("reading input");
+      exit(1);
+    }
+
+    if( sendto(sock, buf, sizeof(buf), 0, (const struct sockaddr *) &server_addr, sizeof(server_addr)) == -1 ) {
+      perror("Sending message to server: %d");
+      fprintf(stderr, "%d\n", errno);
+      fprintf(stderr, "%x\n", server_addr.sin_addr.s_addr);
+      exit(1);
+    }
+
+    printf("message sent: %s", buf);
+  }
+  
+  close(sock);
 }
 
 /*
  * Create a TCP client
  */
-void tcp_client(const unsigned int addr, const unsigned short port) {
+void tcp_client(const unsigned int addr, const unsigned short port, int fd) {
   printf("creating tcp client, dst %d.%d.%d.%d and dport %d\n", addr >> 24, (addr >> 16) & 255, (addr >> 8) & 255,addr & 255, port);
 }
 
